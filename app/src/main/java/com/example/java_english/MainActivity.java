b0c98f2d2b41af70eea2df11f1499f36805bfa46
dana <2023danaj@gmail.com>
2023-08-15 22:36:32 +0900

    2023-08-15

---
+++ app/src/main/java/com/example/java_english/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.java_english;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_move;
+
+    EditText et_id;
+    Button btn_test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , Second.class);
+                startActivity(intent);
+            }
+        });
+
+        et_id = findViewById(R.id.et_id);
+        btn_test = findViewById(R.id.btn_test);
+
+        btn_test.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                et_id.setText("Dana");
+            }
+        });
+    }
+}
